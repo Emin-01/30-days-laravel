9a3cc220b202b5b1f9d898720c8d90bf5f7e5ef4
Emin <337858@student.mboutrecht.nl>
2024-10-06 14:50:41 +0200

    les6

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,50 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director'
+                ,'salary' => '$50.000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer'
+                ,'salary' => '$20.000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher'
+                ,'salary' => '$30.000'
+            ]
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+        $jobs = [
+            [
+                'id' => 1,
+                'title' => 'Director'
+                ,'salary' => '$50.000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer'
+                ,'salary' => '$20.000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher'
+                ,'salary' => '$30.000'
+            ]
+            ];
+
+      $job =  Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
 });
 
 
