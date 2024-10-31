c192e5c1e369b5849db412723381e67e0d7c2254
Emin <337858@student.mboutrecht.nl>
2024-10-31 15:58:47 +0100

    les21

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -8,53 +8,34 @@
 
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
-        <h2 class="text-base/7 font-semibold text-gray-900">Create a new job.</h2>
-        <p class="mt-1 text-sm/6 text-gray-600">We just need a handful detail from you</p>
-  
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-4">
-            <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
-            <div class="mt-2">
-              <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input required type="text" name="title" id="title" autocomplete="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="Shift Leader">
+          <x-form-field>
+              <x-form-label for="title">Title</x-form-label>
+                <div class="mt-2">
+                
+                  <x-form-input name="title" id="title" placeholder="CEO" required/>
+            
+                  <x-form-error name="title" />
+          
               </div>
+          </x-form-field>
 
-              @error('title')
-                    <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-              @enderror
-
+          <x-form-field>
+            <x-form-label for="Salary">Salary</x-form-label>
+              <div class="mt-2">
+              
+                <x-form-input name="salary" id="salary" placeholder="$50.000" required/>
+          
+                <x-form-error name="salary" />
+        
             </div>
-          </div>
-
-           
-          <div class="sm:col-span-4">
-            <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
-            <div class="mt-2">
-              <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input required type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="$50.000 Per Year.">
-              </div>
-              @error('salary')
-                    <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-              @enderror
-            </div>
-          </div>
-
-<!--          <div class="mt-10">
-          @if($errors->any())
-            <ul>
-              @foreach($errors->all() as $error)
-              <li class="text-red-500 italic">{{ $error }}</li>
-              @endforeach
-            </ul>
-          @endif
-          </div>-->
-
+        </x-form-field>
         </div>
       </div>
   
     <div class="mt-6 flex items-center justify-end gap-x-6">
       <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-      <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+      <x-form-button>Save</x-form-button>
     </div>
   </form>
   
